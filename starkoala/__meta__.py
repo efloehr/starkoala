22d2da53dec7d75c41af2ab9c1ba8692e74a4b24
Andrew Kubera <andrewkubera@gmail.com>
2016-10-16 22:54:34 -0400

    Initial starkoala souce code commit.

---
+++ starkoala/__meta__.py
@@ -0,0 +1,21 @@
+#
+# starkoala/__meta__.py
+#
+"""
+Starkoala Metadata
+"""
+
+description = 'A telnet interface for growler applications.'
+
+# update this with new versions
+date = 'Oct 16, 2016'
+
+version_info = (0, 0, 0)
+version = '.'.join(map(str, version_info))
+
+author = 'Eric Floehr, Andrew Kubera'
+author_email = 'left.blank@email.invalid'
+
+url = 'https://github.com/efloehr/starkoala'
+
+license = '???'
