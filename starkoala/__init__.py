12acccd33c738fc88341600554f960aeb0ce8af8
Andrew Kubera <andrewkubera@gmail.com>
2016-10-17 01:07:31 -0400

    Added stub implementations of Protocol, Responder, and Application classes

--- starkoala/__init__.py
+++ starkoala/__init__.py
@@ -2,7 +2,7 @@
 # starkoala/__init__.py
 #
 """
-Asynchronous
+Asynchronous Telnet Server
 """
 
 from .__meta__ import (
@@ -10,3 +10,10 @@ from .__meta__ import (
     author as __author__,
     date as __date__,
 )
+
+from .responder import TelnetResponder
+from .application import Application
+from .protocol import TelnetProtocol
+
+# alias Application as friendlier 'App'
+App = Application
